4188422558045d08af7ec5a40e02b7fb56b4d7be
Artem Motorev <artem-m-14@mail.ru>
2021-10-01 15:55:43 +0700

    First version of rating input

--- app/src/FormView.js
+++ app/src/FormView.js
@@ -33,19 +33,22 @@ let lis = form.map((field) => {
 
                 }else{
                     return (
+                        <div key={field.id}>
+                         <label id={field.id}>{field.text}</label>    
                         <div key={field.id} className="star-rating">
                         <div className="star-rating__wrap">
-                          <input className="star-rating__input" id={`id5-${field.id}`} type="radio" name={field.id} value="5"></input>
-                          <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`id5-${field.id}`} title="5 out of 5 stars"></label>
-                          <input className="star-rating__input" id={`id4-${field.id}`} type="radio" name={field.id} value="4"></input>
-                          <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`id4-${field.id}`} title="4 out of 5 stars"></label>
-                          <input className="star-rating__input" id={`id3-${field.id}`} type="radio" name={field.id} value="3"></input>
-                          <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`id3-${field.id}`} title="3 out of 5 stars"></label>
-                          <input className="star-rating__input" id={`id2-${field.id}`} type="radio" name={field.id} value="2"></input>
-                          <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`id2-${field.id}`} title="2 out of 5 stars"></label>
-                          <input className="star-rating__input" id={`id1-${field.id}`} type="radio" name={field.id} value="1"></input>
-                          <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`id1-${field.id}`} title="1 out of 5 stars"></label>
+                          <input required className="star-rating__input" id={`${field.id}-star-rating-5`} type="radio" name={`rating-${field.id}`} value="5"></input>
+                          <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`${field.id}-star-rating-5`} title="5 out of 5 stars"></label>
+                          <input className="star-rating__input" id={`${field.id}-star-rating-4`} type="radio"  name={`rating-${field.id}`} value="4"></input>
+                          <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`${field.id}-star-rating-4`} title="4 out of 5 stars"></label>
+                          <input className="star-rating__input" id={`${field.id}-star-rating-3`} type="radio"  name={`rating-${field.id}`} value="3"></input>
+                          <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`${field.id}-star-rating-3`} title="3 out of 5 stars"></label>
+                          <input className="star-rating__input" id={`${field.id}-star-rating-2`} type="radio"  name={`rating-${field.id}`} value="2"></input>
+                          <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`${field.id}-star-rating-2`} title="2 out of 5 stars"></label>
+                          <input className="star-rating__input" id={`${field.id}-star-rating-1`} type="radio"  name={`rating-${field.id}`} value="1"></input>
+                          <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`${field.id}-star-rating-1`} title="1 out of 5 stars"></label>
                         </div>
+                      </div>
                       </div>
                     )
                 }
@@ -75,19 +78,23 @@ let lis = form.map((field) => {
 
                 }else{
                     return (
+                    <div key={field.id}>
+                         <label id={field.id}>{field.text}</label>
                         <div key={field.id} className="star-rating">
                         <div className="star-rating__wrap">
-                          <input className="star-rating__input" id={`id5-${field.id}`} type="radio" name={field.id} value="5"></input>
-                          <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`id5-${field.id}`} title="5 out of 5 stars"></label>
-                          <input className="star-rating__input" id={`id4-${field.id}`} type="radio" name={field.id} value="4"></input>
-                          <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`id4-${field.id}`} title="4 out of 5 stars"></label>
-                          <input className="star-rating__input" id={`id3-${field.id}`} type="radio" name={field.id} value="3"></input>
-                          <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`id3-${field.id}`} title="3 out of 5 stars"></label>
-                          <input className="star-rating__input" id={`id2-${field.id}`} type="radio" name={field.id} value="2"></input>
-                          <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`id2-${field.id}`} title="2 out of 5 stars"></label>
-                          <input className="star-rating__input" id={`id1-${field.id}`} type="radio" name={field.id} value="1"></input>
-                          <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`id1-${field.id}`} title="1 out of 5 stars"></label>
+                          <input className="star-rating__input" id={`${field.id}-star-rating-5`} type="radio" name={`rating-${field.id}`} value="5"></input>
+                          <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`${field.id}-star-rating-5`} title="5 out of 5 stars"></label>
+                          <input className="star-rating__input" id={`${field.id}-star-rating-4`} type="radio"  name={`rating-${field.id}`} value="4"></input>
+                          <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`${field.id}-star-rating-4`} title="4 out of 5 stars"></label>
+                          <input className="star-rating__input" id={`${field.id}-star-rating-3`} type="radio"  name={`rating-${field.id}`} value="3"></input>
+                          <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`${field.id}-star-rating-3`} title="3 out of 5 stars"></label>
+                          <input className="star-rating__input" id={`${field.id}-star-rating-2`} type="radio"  name={`rating-${field.id}`} value="2"></input>
+                          <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`${field.id}-star-rating-2`} title="2 out of 5 stars"></label>
+                          <input className="star-rating__input" id={`${field.id}-star-rating-1`} type="radio"  name={`rating-${field.id}`} value="1"></input>
+                          <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`${field.id}-star-rating-1`} title="1 out of 5 stars"></label>
                         </div>
+                        <br/>
+                      </div>
                       </div>
                     )
                 }
