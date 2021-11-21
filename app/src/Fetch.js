f40365551541746689f47dc87fc7495989d17a9a
Artem Motorev <artem-m-14@mail.ru>
2021-11-22 02:58:26 +0700

    All requests are implemented via Fetch

--- app/src/Fetch.js
+++ app/src/Fetch.js
@@ -1,129 +1,191 @@
-const firstUrl = 'http://2665-37-21-158-61.ngrok.io'
+const firstUrl = 'http://localhost:8080'
+const token = 'YourTar ee144968a86f120aa44fc9675911a3f7e658da90c15e37945e46ec82b15c66f0f3d657a0c7bd783122d44127334d6b705ce186325efc2f6f0d501bf16cf631aa'
 
 
-//##############GET##################
-
-export async function getAllForm(){
-    const url = firstUrl+'/api/v1/forms/all'
+//############# FORM ################
+//########## Get All Form ###########
+export async function getAllForm() {
+    const url = firstUrl + '/api/v1/forms/all'
     try {
-        const data = await sendRequestB('GET',url)
+        const data = await sendRequestB('POST', url)
         return data.data
-    } catch(err) { 
+    } catch (err) {
+        alert("Что то пошло не так, повторити попытку")
         console.log(err)
     }
-    
+
 }
-export async function getAllField(){
-    const url = firstUrl+'/api/v1/fields/all'
+
+//########## Find Form ############
+export async function setFindField(id) {
+    const url = firstUrl + 'api/v1/forms/find'
     try {
-        const data = await sendRequestB('GET',url)
+        const data = await sendRequestB('POST', url, { id })
         return data.data
-    } catch(err) { 
+    } catch (err) {
+        alert("Что то пошло не так, повторити попытку")
         console.log(err)
     }
 }
-export async function setAllResponse(){
-}
 
-
-//##############POST##################
-
-export async function setAddForm(body){
-    const url = firstUrl+'/api/v1/forms/add'
+//########## Add Form ############
+export async function setAddForm(form) {
+    const url = firstUrl + '/api/v1/forms/add'
     try {
-        const data = await sendRequestB('POST',url,body)
-        alert(data.message)
-    } catch(err) { 
-        console.log(err)
-    }
-}
-export async function setAddField(field){
-    console.log(field);
-    const url = firstUrl+'/api/v1/fields/add'
-    try {
-        const data = await sendRequestB('POST',url,field)
+        const data = await sendRequestB('POST', url, form)
         return data.data
-    } catch(err) { 
+    } catch (err) {
+        alert("Что то пошло не так, повторити попытку")
         console.log(err)
     }
 }
-export async function setAddResponse(response){
+
+//########## Delete Form ############
+export async function setDeleteForm(id) {
+    const url = firstUrl + '/api/v1/forms/remove'
+    try {
+        const data = await sendRequestB('DELETE', url, { id })
+        return data.data
+    } catch (err) {
+        alert("Что то пошло не так, повторити попытку")
+        console.log(err)
+    }
 }
 
-
-//##############DELETE##################
-
-export async function setDeleteForm(body){
-    const url = firstUrl+'/api/v1/forms/remove'
+//########## Update Form ############
+export async function setUpdateForm(form) {
+    const url = firstUrl + '/api/v1/forms/update'
     try {
-        const data = await sendRequestB('DELETE',url,body)
-        alert(data.message)
+        const data = await sendRequestB('PATCH', url, form)
+        return data.data
+    } catch (err) {
+        alert("Что то пошло не так, повторити попытку")
+        console.log(err)
+    }
+}
+
+//############# FIELD ################
+//########## Get All Field ###########
+export async function getAllField() {
+    const url = firstUrl + '/api/v1/fields/all'
+    try {
+        const data = await sendRequestB('POST', url)
+        return data.data
+    } catch (err) {
+        alert("Что то пошло не так, повторити попытку")
+        console.log(err)
+    }
+}
+
+//########## Find Field ############
+export async function FindField(id) {
+    const url = firstUrl + '/api/v1/fields/find'
+    try {
+        const data = await sendRequestB('POST', url, { id })
+        return data.data
+    } catch (err) {
+        alert("Что то пошло не так, повторити попытку")
+        console.log(err)
+    }
+}
+
+//########## Add Field ############
+export async function setAddField(field) {
+    const url = firstUrl + '/api/v1/fields/add'
+    try {
+        const data = await sendRequestB('POST', url, field)
+        return data.data
+    } catch (err) {
+        alert("Что то пошло не так, повторити попытку")
+        console.log(err)
+    }
+}
+
+//########## Delete Field ############
+export async function setDeleteField(id) {
+    const url = firstUrl + '/api/v1/fields/remove'
+    try {
+        const data = await sendRequestB('DELETE', url, { id })
         return data.message
-    } catch(err) { 
-        console.log(err)
-    }
-}
-export async function setDeleteField(id){
-    const url = firstUrl+'/api/v1/fields/remove'
-    try {
-        const data = await sendRequestB('DELETE',url,{id})
-        alert(data.message)
-        return data.message
-    } catch(err) { 
-        console.log(err)
-    }
-}
-export async function setDeleteResponse(id){
-}
-
-
-//##############PATCH##################
-
-export async function setUpdateForm(body){
-    const url = firstUrl+'/api/v1/forms/update'
-    try {
-        const data = await sendRequestB('PATCH',url,body)
-        alert(data.message)
-    } catch(err) { 
-        console.log(err)
-    }
-}
-export async function setUpdateField(field){
-    const url = firstUrl+'/api/v1/fields/update'
-    try {
-        const data = await sendRequestB('PATCH',url,field)
-        alert(data.message)
-    } catch(err) { 
+    } catch (err) {
+        alert("Что то пошло не так, повторити попытку")
         console.log(err)
     }
 }
 
-
-//##############GET-ID##################
-
-export async function setFindField(id){
-    const url = firstUrl+'/api/v1/fields/find'
+//########## Update Field ############
+export async function setUpdateField(field) {
+    const url = firstUrl + '/api/v1/fields/update'
     try {
-        const data = await sendRequestB('GET',url,{id})
+        const data = await sendRequestB('PATCH', url, field)
         return data.data
-    } catch(err) { 
+    } catch (err) {
+        alert("Что то пошло не так, повторити попытку")
+        console.log(err)
+    }
+}
+
+//########## Get All Field Form ###########
+export async function getFieldForm(id) {
+    const url = firstUrl + '/api/v1/forms/find/fields'
+    try {
+        const data = await sendRequestB('POST', url, { id })
+        return data.data
+    } catch (err) {
+        alert("Что то пошло не так, повторити попытку")
         console.log(err)
     }
 }
 
 
+//############# RESPONSE ################
+//########## Get All Response Form ############
+export async function getAllResponse() {
+    const url = firstUrl + '/api/v1/response/get'
+    try {
+        const data = await sendRequestB('POST', url)
+        return data.data
+    } catch (err) {
+        alert("Что то пошло не так, повторити попытку")
+        console.log(err)
+    }
+}
 
+//########## Add Response ############
+export async function setAddResponse(response) {
+    const url = firstUrl + '/api/v1/response/add'
+    try {
+        const data = await sendRequestB('POST', url, response)
+        return data.data
+    } catch (err) {
+        alert("Что то пошло не так, повторити попытку")
+        console.log(err)
+    }
+}
 
-export async function sendRequestB(method, url, body=undefined) {
+//########## Delete Response ############
+export async function setDeleteResponse(id) {
+    const url = firstUrl + '/api/v1/response/remove'
+    try {
+        const data = await sendRequestB('POST', url, { id })
+        return data.data
+    } catch (err) {
+        alert("Что то пошло не так, повторити попытку")
+        console.log(err)
+    }
+}
+
+export async function sendRequestB(method, url, body = undefined) {
     const headers = {
-        'YT-AUTH-TOKEN': 'YourTar ee144968a86f120aa44fc9675911a3f7e658da90c15e37945e46ec82b15c66f0f3d657a0c7bd783122d44127334d6b705ce186325efc2f6f0d501bf16cf631aa',
+        'YT-AUTH-TOKEN': token,
         'Content-Type': 'application/json',
     }
-  const response = await fetch(url, {
+    const response = await fetch(url, {
         method: method,
         headers: headers,
         body: JSON.stringify(body)
     })
     return await response.json()
 }
+
 export default getAllForm
