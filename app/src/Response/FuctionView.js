38638c2b838775e0c5ecc88242db27f7315ebf61
Artem Motorev <artem-m-14@mail.ru>
2021-11-25 02:34:57 +0700

    Update function for view. For view response

--- app/src/Response/FuctionView.js
+++ app/src/Response/FuctionView.js
@@ -1,13 +1,23 @@
 import propTypes from 'prop-types'
+import AppState from '../AppState'
+import { setDeleteResponse } from '../Fetch'
 
-export function MoreParams(field) {
+export function delelResponse(id) {
+    AppState.delDataState(id)
+    return setDeleteResponse(id)
+}
+delelResponse.propTypes = {
+    id: propTypes.number
+}
+
+export function MoreParams(field, response = false, id) {
     return <div key={field.id}>
         {field.title}
         <br />
         {field.possbleValues.map((titleValue) => {
             return (
-                <label key={titleValue.id} id={field.id}>
-                    <input name={`${field.id}`} required={field.isRequire} id={field.id} type={field.inputType} value={titleValue.title}/>
+                <label key={titleValue.id} id={response === true? `${field.id}${id}`:field.id}>
+                    <input defaultChecked={field.responseBody.value === titleValue.title? true: false} disabled={response} name={response === true? `${field.id}${id}`:field.id} required={field.isRequire} id={response === true? `${field.id}${id}`:field.id} type={field.inputType} value={titleValue.title}/>
                     {titleValue.title}
                 </label>
             )
@@ -15,38 +25,46 @@ export function MoreParams(field) {
     </div>
 }
 
-export function defParams(field) {
+export function defParams(field, response = false) {
+    let valueThis = undefined
+    if(response){
+     valueThis = field.responseBody.value
+    }
     return <div key={field.id}>
         <label id={field.id}>{field.title}
             <br />
-            <input name={`${field.id}`} placeholder={field.placeHolder} required={field.isRequire} id={field.id} type={field.inputType} />
+            <input value={valueThis} disabled={response} name={`${field.id}`} placeholder={field.placeHolder} required={field.isRequire} id={field.id} type={field.inputType} />
         </label>
     </div>
 }
 
-export function TextArea(field) {
+export function TextArea(field, response = false) {
+    let valueThis = undefined
+    if(response){
+     valueThis = field.responseBody.value
+    }
     return <div key={field.id}>
         <label id={field.id}>{field.title}
             <br />
-            <textarea name={`${field.id}`} placeholder={field.placeHolder} required={field.isRequire} id={field.id} />
+            <textarea value={valueThis} disabled={response} name={`${field.id}`} placeholder={field.placeHolder} required={field.isRequire} id={field.id} />
         </label>
     </div>
 }
 
-export function Rating(field) {
+export function Rating(field, response = false,  value = undefined) {
     return <div key={field.id}>
         <label id={field.id}>{field.title}</label>
         <div className="star-rating">
             <div className="star-rating__wrap">
-                <input required={field.isRequire} className="star-rating__input" id={`${field.id}-star-rating-5`} type="radio" name={`${field.id}`} value="5" />
+                <input disabled={response} required={field.isRequire} className="star-rating__input" id={`${field.id}-star-rating-5`} type="radio" name={`${field.id}`} value="5" />
                 <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`${field.id}-star-rating-5`} title="5 out of 5 stars"></label>
-                <input className="star-rating__input" id={`${field.id}-star-rating-4`} type="radio" name={`${field.id}`} value="4" />
+                <input disabled={response} className="star-rating__input" id={`${field.id}-star-rating-4`} type="radio" name={`${field.id}`} value="4" />
                 <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`${field.id}-star-rating-4`} title="4 out of 5 stars"></label>
-                <input className="star-rating__input" id={`${field.id}-star-rating-3`} type="radio" name={`${field.id}`} value="3" />
+                <input disabled={response} className="star-rating__input" id={`${field.id}-star-rating-3`} type="radio" name={`${field.id}`} value="3" />
                 <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`${field.id}-star-rating-3`} title="3 out of 5 stars"></label>
-                <input className="star-rating__input" id={`${field.id}-star-rating-2`} type="radio" name={`${field.id}`} value="2" />
+                <input disabled={response} className="star-rating__input" id={`${field.id}-star-rating-2`} type="radio" name={`${field.id}`} value="2" />
                 <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`${field.id}-star-rating-2`} title="2 out of 5 stars"></label>
-                <input className="star-rating__input" id={`${field.id}-star-rating-1`} type="radio" name={`${field.id}`} value="1" />
+                <input disabled={response} className="star-rating__input" id={`${field.id}-star-rating-1`} type="radio" name={`${field.id}`} value="1" />
                 <label className="star-rating__ico fa fa-star-o fa-lg" htmlFor={`${field.id}-star-rating-1`} title="1 out of 5 stars"></label>
             </div>
         </div>
