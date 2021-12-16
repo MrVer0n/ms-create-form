d63337d9ae61429b38fd47a17211ecf68585afe9
Artem Motorev <artem-m-14@mail.ru>
2021-12-17 01:48:40 +0700

    Add styles for form

--- app/src/Forms/AddForm.js
+++ app/src/Forms/AddForm.js
@@ -19,31 +19,42 @@ function AddForm(props) {
     }
     return (
         <div>
-            <h3>Создать форму</h3>
-            <label id="nameForm">Название формы:
-                <br />
-                <input
-                    className="nameForm"
-                    id="nameForm"
-                    type="text"
-                    value={form.name}
-                    onChange={e => setForm(inputName(e.target.value))} />
-            </label>
-            <br />
+            <div>
+                <i className="float-left medium material-icons">create_new_folder</i>
+                <h2 className='margin-t text-cursor-off'>Создание формы</h2>
+                <hr />
+            </div>
+            <div className="card-panel hoverable add-form col s12 m2 z-depth-4">
+                <div className="col s12 m6">
+                    <div className="card blue-grey darken-1">
+                        <div className="card-content white-text">
+                            <label id="nameForm">Название формы:
+                                <input
+                                    className="nameForm white-text"
+                                    id="nameForm"
+                                    type="text"
+                                    value={form.name}
+                                    onChange={e => setForm(inputName(e.target.value))} />
+                            </label>
+                        </div>
 
-            <label id="nameForm">Описание формы:
-                <br />
-                <input
-                    className="titleForm"
-                    id="titleForm"
-                    type="text"
-                    value={form.title}
-                    onChange={e => setForm(inputTitle(e.target.value))} />
-            </label>
-            <br />
-
-            <button onClick={() => props.history.goBack()}>Отмена</button>
-            <button onClick={async () => { await addForm(form); props.history.goBack() }}>Добавить форму</button>
+                        <div className="card-action">
+                            <label id="nameForm">Описание формы:
+                                <input
+                                    className="titleForm white-text"
+                                    id="titleForm"
+                                    type="text"
+                                    value={form.title}
+                                    onChange={e => setForm(inputTitle(e.target.value))} />
+                            </label>
+                        </div>
+                        <div className="card-action right-button">
+                            <button className='waves-effect waves-light btn #009688 teal' onClick={() => props.history.goBack()}>Отмена</button>
+                            <button className='waves-effect waves-light btn left-pad #009688 teal' onClick={async () => { await addForm(form); props.history.goBack() }}>Добавить форму</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
